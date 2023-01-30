ea2f145c4caac1e9a2f29fcdf49a139cc0291783
corchessergiu <corchessergiu194@gmail.com>
2023-01-30 16:35:53 +0200

    update tests

--- test/ClaimFeesRewards/test_claimFees.js
+++ test/ClaimFeesRewards/test_claimFees.js
@@ -39,4 +39,121 @@ describe("Test claim fee functionality", async function() {
         deanInstance = XENContract.connect(dean);
         carolInstance = XENContract.connect(carol);
     });
+
+    it("Claim fees", async() => {
+        const lib = await ethers.getContractFactory("MathX");
+        const libraryLocal = await lib.deploy();
+
+        const xenContractLocal = await ethers.getContractFactory("XENCrypto", {
+            libraries: {
+                MathX: libraryLocal.address
+            }
+        });
+
+        XENContractLocal = await xenContractLocal.deploy();
+        await XENContractLocal.deployed();
+
+        aliceInstance = XENContractLocal.connect(alice);
+        bobInstance = XENContractLocal.connect(bob);
+        deanInstance = XENContractLocal.connect(dean);
+        carolInstance = XENContractLocal.connect(carol);
+
+        await aliceInstance.claimRank(100);
+        await bobInstance.claimRank(100);
+        await carolInstance.claimRank(100);
+        await deanInstance.claimRank(100);
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 102 * 24])
+        await hre.ethers.provider.send("evm_mine")
+        await aliceInstance.claimMintReward();
+        await bobInstance.claimMintReward();
+        await carolInstance.claimMintReward();
+        await deanInstance.claimMintReward();
+
+        const DBXenLocal = await ethers.getContractFactory("DBXen");
+        DBXenContractLocal = await DBXenLocal.deploy(ethers.constants.AddressZero, XENContractLocal.address);
+        await DBXenContractLocal.deployed();
+
+        const Deb0xViewsLocal = await ethers.getContractFactory("DBXenViews");
+        DBXENViewContractLocal = await Deb0xViewsLocal.deploy(DBXenContractLocal.address);
+        await DBXENViewContractLocal.deployed();
+
+        const dbxAddress = await DBXenContractLocal.dxn()
+        DBXenERC20 = new ethers.Contract(dbxAddress, abi, hre.ethers.provider)
+
+        await XENContractLocal.connect(alice).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(alice).burnBatch(1, { value: ethers.utils.parseEther("1") })
+
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
+        await hre.ethers.provider.send("evm_mine")
+
+        let unclaimedFeesAlice = await DBXENViewContractLocal.getUnclaimedFees(alice.address);
+
+        await DBXenContractLocal.connect(alice).claimFees();
+        const feesClaimed = await DBXenContractLocal.queryFilter("FeesClaimed")
+        let totalFeesClaimed = BigNumber.from("0")
+        for (let entry of feesClaimed) {
+            totalFeesClaimed = totalFeesClaimed.add(entry.args.fees)
+        }
+        expect(unclaimedFeesAlice).to.equal(totalFeesClaimed);
+
+        await XENContractLocal.connect(bob).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(bob).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
+        await hre.ethers.provider.send("evm_mine")
+
+        let unclaimedFeesBob = await DBXENViewContractLocal.getUnclaimedFees(bob.address);
+        await DBXenContractLocal.connect(bob).claimFees();
+        const feesClaimedForBob = await DBXenContractLocal.queryFilter("FeesClaimed")
+        let totalFeesClaimedForBob = BigNumber.from("0")
+        for (let entry of feesClaimedForBob) {
+            totalFeesClaimedForBob = totalFeesClaimedForBob.add(entry.args.fees)
+        }
+        expect(unclaimedFeesBob).to.equal(BigNumber.from(totalFeesClaimedForBob).sub(BigNumber.from(unclaimedFeesAlice)));
+
+        await XENContractLocal.connect(bob).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(bob).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24]);
+        await hre.ethers.provider.send("evm_mine");
+
+        await XENContractLocal.connect(alice).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(alice).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+        await XENContractLocal.connect(bob).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(bob).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+        await XENContractLocal.connect(dean).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(dean).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+        await XENContractLocal.connect(carol).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(carol).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24]);
+        await hre.ethers.provider.send("evm_mine");
+
+        let unclaimedFeesBobCurrent = await DBXENViewContractLocal.getUnclaimedFees(bob.address);
+        let unclaimedFeesAliceCurrent = await DBXENViewContractLocal.getUnclaimedFees(alice.address);
+        let unclaimedFeesDeanCurrent = await DBXENViewContractLocal.getUnclaimedFees(dean.address);
+        let unclaimedFeesCarolCurrent = await DBXENViewContractLocal.getUnclaimedFees(carol.address);
+
+        await DBXenContractLocal.connect(alice).claimFees();
+        await DBXenContractLocal.connect(bob).claimFees();
+        await DBXenContractLocal.connect(dean).claimFees();
+        await DBXenContractLocal.connect(carol).claimFees();
+
+        let feeForActualCycle = BigNumber.from(unclaimedFeesBobCurrent).add(BigNumber.from(unclaimedFeesAliceCurrent)).add(BigNumber.from(unclaimedFeesDeanCurrent)).add(BigNumber.from(unclaimedFeesCarolCurrent))
+
+        const feesClaimed2 = await DBXenContractLocal.queryFilter("FeesClaimed")
+        let totalFeesClaimedForAll = BigNumber.from("0")
+        for (let entry of feesClaimed2) {
+            totalFeesClaimedForAll = totalFeesClaimedForAll.add(entry.args.fees)
+        }
+        expect(BigNumber.from(totalFeesClaimedForAll).sub(BigNumber.from(totalFeesClaimedForBob))).to.equal(feeForActualCycle);
+
+        await XENContractLocal.connect(alice).approve(DBXenContractLocal.address, ethers.utils.parseEther("500000"))
+        await DBXenContractLocal.connect(alice).burnBatch(9900, { value: ethers.utils.parseEther("100") })
+
+    });
+
+
 });
