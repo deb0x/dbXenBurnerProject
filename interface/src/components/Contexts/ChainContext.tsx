bfd23402a305e82aa73232f830d0c72f16b736c3
Alexhandru <alextitieni@yahoo.com>
2023-03-02 17:06:12 +0200

    New DBXenViews deployment addresses

--- interface/src/components/Contexts/ChainContext.tsx
+++ interface/src/components/Contexts/ChainContext.tsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const initialChain = {
     chain: {
         deb0xAddress: "0x4F3ce26D9749C0f36012C9AbB41BF9938476c462",
-        deb0xViewsAddress: "0xCF7582E5FaC8a6674CcD96ce71D807808Ca8ba6E",
+        deb0xViewsAddress: "0xE8696A871C5eaB13bA566A4C15b8144AFeEAFfbA",
         deb0xERC20Address: "0x47DD60FA40A050c0677dE19921Eb4cc512947729",
         xenCryptoAddress: "0x2AB0e9e4eE70FFf1fB9D67031E44F6410170d00e",
         chainId: "137",
--- interface/src/components/Contexts/ChainProvider.tsx
+++ interface/src/components/Contexts/ChainProvider.tsx
@@ -16,7 +16,7 @@ const ChainProvider = ( { children }: Props ) => {
                 if(parseInt(result, 16) === 137) {
                     setChain({
                         deb0xAddress: "0x4F3ce26D9749C0f36012C9AbB41BF9938476c462",
-                        deb0xViewsAddress: "0xCF7582E5FaC8a6674CcD96ce71D807808Ca8ba6E",
+                        deb0xViewsAddress: "0xE8696A871C5eaB13bA566A4C15b8144AFeEAFfbA",
                         deb0xERC20Address: "0x47DD60FA40A050c0677dE19921Eb4cc512947729",
                         xenCryptoAddress: "0x2AB0e9e4eE70FFf1fB9D67031E44F6410170d00e",
                         chainId: parseInt(result, 16),
@@ -26,7 +26,7 @@ const ChainProvider = ( { children }: Props ) => {
                 } else {
                     setChain({
                         deb0xAddress: "0xF5c80c305803280B587F8cabBcCdC4d9BF522AbD",
-                        deb0xViewsAddress: "0xf032f7FB8258728A1938473B2115BB163d5Da593",
+                        deb0xViewsAddress: "0x67873aDDc934C6A1C4b2Bd6d2e08D4431d1181fD",
                         deb0xERC20Address: "0x80f0C1c49891dcFDD40b6e0F960F84E6042bcB6F",
                         xenCryptoAddress: "0xC0C5AA69Dbe4d6DDdfBc89c0957686ec60F24389",
                         chainId: parseInt(result, 16),
