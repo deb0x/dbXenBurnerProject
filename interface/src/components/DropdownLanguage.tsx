4d5996a1e7a588c7ca2e2453430e0274f985b536
csilla.andras <csilla.andras@infinitpartners.com>
2023-03-06 11:44:43 +0200

    Translation modifications

--- interface/src/components/DropdownLanguage.tsx
+++ interface/src/components/DropdownLanguage.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const DropdownLanguage = () => {
-    const { i18n } = useTranslation();
-    const [language, setLanguage] = useState(JSON.parse(localStorage.getItem('language') || "en" ));
+    const { i18n, t } = useTranslation();
+    const [language, setLanguage] = useState(JSON.parse(localStorage.getItem('language') || "null" ));
 
     useEffect(() => {
         localStorage.setItem('language', JSON.stringify(language));
@@ -16,9 +16,9 @@ const DropdownLanguage = () => {
     };
 
     return (
-        <select onChange={handleLangChange} value={language}>
-            <option value="zh">ZH</option>
-            <option value="en">EN</option>
+        <select onChange={handleLangChange} value={language} className="language-switcher">
+            <option value="en">{t("app_bar.language.english")}</option>
+            <option value="zh">{t("app_bar.language.chinese")}</option>
         </select>
     );
 };
