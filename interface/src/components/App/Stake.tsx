e33c173768e82c94b03267069e7506291e3ecc78
corchessergiu <corchessergiu194@gmail.com>
2023-03-22 14:37:19 +0200

    Add date

--- interface/src/components/App/Stake.tsx
+++ interface/src/components/App/Stake.tsx
@@ -38,7 +38,7 @@ export function Stake(props: any): any {
     const [notificationState, setNotificationState] = useState({})
     const gaEventTracker = useAnalyticsEventTracker('Stake');
     const [previousCycleXENBurned, setPreviousCycleXENBurned] = useState<any>();
-    const dateEthereum: any = new Date(Date.UTC(2023, 3, 17, 14, 3, 19, 0));
+    const dateEthereum: any = new Date(Date.UTC(2023, 3, 17, 12, 24, 59, 0));
     const datePolygon: any = new Date(Date.UTC(2023, 3, 17, 14, 3, 19, 0));
     const dateAvalanche: any = new Date(Date.UTC(2023, 3, 17, 14, 7, 20, 0));
     const dateBinance: any = new Date(Date.UTC(2023, 3, 17, 13, 57, 40, 0));
@@ -81,7 +81,7 @@ export function Stake(props: any): any {
     function timer() {
         switch(Number(chain.chainId)) {
             case 1: 
-                setEndDate(dateEthereumPow.getTime() - now);
+                setEndDate(dateEthereum.getTime() - now);
                 break;
             case 137: 
                 setEndDate(datePolygon.getTime() - now);
