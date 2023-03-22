72f6dc974659e18acfa191d3bd3353b8298cba00
corchessergiu <corchessergiu194@gmail.com>
2023-03-22 18:57:47 +0200

    update eth rpc

--- interface/src/components/Contexts/ChainSetter.tsx
+++ interface/src/components/Contexts/ChainSetter.tsx
@@ -121,7 +121,7 @@ const networks: any = {
             symbol: 'ETH',
             decimals: 18
         },
-        rpcUrls: ['https://eth.llamarpc.com'],
+        rpcUrls: ['https://mainnet.infura.io/v3/'],
         blockExplorerUrls: ['https://etherscan.io'] 
     }
 };
