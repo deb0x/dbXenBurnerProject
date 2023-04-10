bac12dce6bfc20df176c8bf4c61d039e0b454788
corchessergiu <corchessergiu194@gmail.com>
2023-04-10 12:10:41 +0300

    update chain provider

--- interface/src/components/Contexts/ChainProvider.tsx
+++ interface/src/components/Contexts/ChainProvider.tsx
@@ -19,12 +19,12 @@ const ChainProvider = ( { children }: Props ) => {
                             deb0xAddress: "0xF5c80c305803280B587F8cabBcCdC4d9BF522AbD",
                             deb0xViewsAddress: "0xf032f7FB8258728A1938473B2115BB163d5Da593",
                             deb0xERC20Address: "0x80f0C1c49891dcFDD40b6e0F960F84E6042bcB6F",
-                            xenCryptoAddress: "0xD342D63466B520d8D331CaFF863900d402Aa5b00",
+                            xenCryptoAddress: "0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8",
                             chainId: parseInt(result, 16),
-                            chainName: "X1 Devnet",
-                            currency: "XN",
-                            priceURL: "https://explorer.x1-devnet.xen.network/",
-                            dxnTokenName: "xnDXN"
+                            chainName: "Ethereum",
+                            currency: "ETH",
+                            priceURL: "https://mainnet.infura.io/v3/6010818c577b4531b1886965421a91d3",
+                            dxnTokenName: "DXN"
                         })
                         break;
                     case 202212: 
@@ -36,7 +36,7 @@ const ChainProvider = ( { children }: Props ) => {
                             chainId: parseInt(result, 16),
                             chainName: "X1 Devnet",
                             currency: "XN",
-                            priceURL: "https://mainnet.infura.io/v3/6010818c577b4531b1886965421a91d3",
+                            priceURL: "https://explorer.x1-devnet.xen.network/",
                             dxnTokenName: "xnDXN"
                         })
                         break;
