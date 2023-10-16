b8d49e648f905a415646904d6d796766f2008489
sergiu.corches <corchessergiu194@gmail.com>
2023-10-16 13:30:37 +0300

    update cron

--- scripts/cron_base.js
+++ scripts/cron_base.js
@@ -1,11 +1,11 @@
-import cron from 'node-cron';
-import { JsonRpcProvider } from 'ethers';
-import { formatEther } from 'ethers';
+import cron from "node-cron";
+import { JsonRpcProvider } from "ethers";
+import { formatEther } from "ethers";
 import Factory from "./dbxenftFactory.js";
 import mintInfo from "./mintInfo.js";
 import XENFT from "./DBXENFT.js";
-import dotenv from 'dotenv';
-import BigNumber from 'bignumber.js';
+import dotenv from "dotenv";
+import BigNumber from "bignumber.js";
 import Moralis from "moralis";
 import fetch from "node-fetch";
 dotenv.config();
@@ -15,195 +15,165 @@ const mintInfoAddress = "0xF8755609b6596CEE3A8CeAF8D9cFD90E7479C46A";
 const xenftAddress = "0x94d9e02d115646dfc407abde75fa45256d66e043";
 
 const STORAGE_EP = "https://dbxen-be.prodigy-it-solutions.com/api/storage/";
-const METADATA_BUCKET_BASE = "deboxnft-minting-base"
+const METADATA_BUCKET_BASE = "deboxnft-minting-base";
 
-const createApiOptions = (data) =>
-    ({ method: "POST", body: JSON.stringify(data) });
+const createApiOptions = (data) => ({
+  method: "POST",
+  body: JSON.stringify(data),
+});
 
 const getStorageObject = (data) =>
-    fetch(STORAGE_EP + "GetObjectCommand", createApiOptions(data))
-    .then((result) => result.json());
+  fetch(
+    STORAGE_EP + "GetObjectCommand",
+    createApiOptions(data)
+  ).then((result) => result.json());
 
 const putStorageObject = (data) =>
-    fetch(STORAGE_EP + "PutObjectCommand", createApiOptions(data))
-    .then((result) => result.json());
+  fetch(
+    STORAGE_EP + "PutObjectCommand",
+    createApiOptions(data)
+  ).then((result) => result.json());
 
 function subMinutes(date, minutes) {
-    date.setMinutes(date.getMinutes() - minutes);
-    return date;
+  date.setMinutes(date.getMinutes() - minutes);
+  return date;
 }
 
 function mulDiv(x, y, denominator) {
-    const bx = new BigNumber(x);
-    const by = new BigNumber(y);
-    const bDenominator = new BigNumber(denominator);
-    const result = bx.times(by).dividedBy(bDenominator);
+  const bx = new BigNumber(x);
+  const by = new BigNumber(y);
+  const bDenominator = new BigNumber(denominator);
+  const result = bx.times(by).dividedBy(bDenominator);
 
-    return result.toString();
+  return result.toString();
 }
 
-async function getLast24HoursIdsMinted() {
-    
-}
+async function getLast24HoursIdsMinted() {}
 
-async function getIdsFromEvent(cursor, dateForParam) {
- 
-}
+async function getIdsFromEvent(cursor, dateForParam) {}
 
 async function generateAfterReveal() {
-    const provider = new JsonRpcProvider(`https://base-mainnet.gateway.pokt.network/v1/lb/${process.env.REACT_APP_POKT_KEY}`);
-    let factory = Factory(provider, dbxenftFactoryAddress);
-    let ids = await getLast24HoursIdsMinted();
-    const MintInfoContract = mintInfo(provider, mintInfoAddress);
-    const XENFTContract = XENFT(provider, xenftAddress);
-    let counter1 = 1;
-    let counter2 = 1;
-    let counter3 = 1;
-    let counter4 = 1;
-    let counter5 = 1;
-    let counter6 = 1;
-    let counter7 = 1;
-    let counter8 = 1;
-    let counter9 = 1;
-    for (let i = ids.length - 1; i >= 0; i--) {
-        let XENFTID = Number(await factory.dbxenftUnderlyingXENFT(ids[i]));
-        let mintInforesult = await XENFTContract.mintInfo(XENFTID);
-        let mintInfoData = await MintInfoContract.decodeMintInfo(mintInforesult);
-        let maturityTs = Number(mintInfoData[1]);
-        let fileName = ids[i] + ".json";
-        let tokenEntryCycle = Number(await factory.tokenEntryCycle(ids[i]));
-        let dbxenftEntryPower = formatEther((await factory.dbxenftEntryPower(ids[i])))
-        let rewardPerCycle = formatEther(await factory.rewardPerCycle(tokenEntryCycle))
-        let totalEntryPowerPerCycle = formatEther(await factory.totalEntryPowerPerCycle(tokenEntryCycle))
-        let newPower = mulDiv(dbxenftEntryPower.toString(), rewardPerCycle.toString(), totalEntryPowerPerCycle.toString())
+  const provider = new JsonRpcProvider(
+    `https://base-mainnet.gateway.pokt.network/v1/lb/${process.env.REACT_APP_POKT_KEY}`
+  );
+  let factory = Factory(provider, dbxenftFactoryAddress);
+  let ids = await getLast24HoursIdsMinted();
+  const MintInfoContract = mintInfo(provider, mintInfoAddress);
+  const XENFTContract = XENFT(provider, xenftAddress);
 
-        try {
-            let attributesValue = [{
-                "trait_type": "DBXEN NFT POWER",
-                "value": newPower.toString()
-            }, {
-                "trait_type": "ESTIMATED XEN",
-                "value": dbxenftEntryPower.toString(),
-            }, {
-                "trait_type": "MATURITY DATE",
-                "value": new Date(maturityTs * 1000).toString(),
-            }, ]
-            let result = getImage(newPower, counter1, counter2, counter3, counter4, counter5, counter6, counter7, counter8, counter9);
-            let standardMetadata = {
-                "id": `${ids[i]}`,
-                "name": `#${ids[i]} DBXeNFT: Cool art & Trustless Daily Yield`,
-                "description": "",
-                "image": result.link,
-                "external_url": `https://dbxen.org/your-dbxenfts/${METADATA_BUCKET_BASE}/${ids[i]}`,
-                "attributes": attributesValue
-            }
+  for (let i = ids.length - 1; i >= 0; i--) {
+    let XENFTID = Number(await factory.dbxenftUnderlyingXENFT(ids[i]));
+    let mintInforesult = await XENFTContract.mintInfo(XENFTID);
+    let mintInfoData = await MintInfoContract.decodeMintInfo(mintInforesult);
+    let maturityTs = Number(mintInfoData[1]);
+    let fileName = ids[i] + ".json";
+    let tokenEntryCycle = Number(await factory.tokenEntryCycle(ids[i]));
+    let dbxenftEntryPower = formatEther(
+      await factory.dbxenftEntryPower(ids[i])
+    );
+    let rewardPerCycle = formatEther(
+      await factory.rewardPerCycle(tokenEntryCycle)
+    );
+    let totalEntryPowerPerCycle = formatEther(
+      await factory.totalEntryPowerPerCycle(tokenEntryCycle)
+    );
+    let newPower = mulDiv(
+      dbxenftEntryPower.toString(),
+      rewardPerCycle.toString(),
+      totalEntryPowerPerCycle.toString()
+    );
 
-            console.log(JSON.stringify(standardMetadata));
+    try {
+      let attributesValue = [
+        {
+          trait_type: "DBXEN NFT POWER",
+          value: newPower.toString(),
+        },
+        {
+          trait_type: "ESTIMATED XEN",
+          value: dbxenftEntryPower.toString(),
+        },
+        {
+          trait_type: "MATURITY DATE",
+          value: new Date(maturityTs * 1000).toString(),
+        },
+      ];
+      let result = getImage(newPower, Number(ids[i]));
+      let standardMetadata = {
+        id: `${ids[i]}`,
+        name: `#${ids[i]} DBXeNFT: Cool art & Trustless Daily Yield`,
+        description: "",
+        image: result,
+        external_url: `https://dbxen.org/your-dbxenfts/${METADATA_BUCKET_BASE}/${ids[i]}`,
+        attributes: attributesValue,
+      };
 
-            switch (result.value) {
-                case 1:
-                    if (counter1 + 1 > 10000)
-                        counter1 = 1;
-                    else
-                        counter1++
-                        break;
-                case 2:
-                    if (counter2 + 1 > 10000)
-                        counter2 = 1
-                    else
-                        counter2++
-                        break;
-                case 3:
-                    if (counter3 + 1 > 10000)
-                        counter3 = 1
-                    else
-                        counter3++
-                        break;
-                case 4:
-                    if (counter4 + 1 > 10000)
-                        counter4 = 1
-                    else
-                        counter4++
-                        break;
-                case 5:
-                    if (counter5 + 1 > 10000)
-                        counter5 = 1
-                    else
-                        counter5++
-                        break;
-                case 6:
-                    if (counter6 + 1 > 6000)
-                        counter6 = 1
-                    else
-                        counter6++
-                        break;
-                case 7:
-                    if (counter7 + 1 > 6000)
-                        counter7 = 1
-                    else
-                        counter7++
-                        break;
-                case 8:
-                    if (counter8 + 1 > 3500)
-                        counter8 = 1
-                    else
-                        counter8++
-                        break;
-                case 9:
-                    if (counter9 + 1 > 2500)
-                        counter9 = 1
-                    else
-                        counter9++
-                        break;
-                default:
-                    console.log("Default case")
-                    break;
-            }
-            const params = {
-                Bucket: METADATA_BUCKET_BASE,
-                Key: fileName,
-                Body: JSON.stringify(standardMetadata),
-                Tagging: 'public=yes',
-                "ContentType": "application/json",
-            };
+      console.log(JSON.stringify(standardMetadata));
 
-            putStorageObject(params)
-                .then((result) => {
-                    console.log(result)
-                }).catch((error) => console.log(error));
+      const params = {
+        Bucket: METADATA_BUCKET_BASE,
+        Key: fileName,
+        Body: JSON.stringify(standardMetadata),
+        Tagging: "public=yes",
+        ContentType: "application/json",
+      };
 
-        } catch (err) {
-            console.log(err)
-            if (err.client_error.Code == "NoSuchKey") {
-                console.log("ERROR AT UPDATE!!!")
-            } else {
-                throw err;
-            }
-        }
+      putStorageObject(params)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.log(error));
+    } catch (err) {
+      console.log(err);
+      if (err.client_error.Code == "NoSuchKey") {
+        console.log("ERROR AT UPDATE!!!");
+      } else {
+        throw err;
+      }
     }
+  }
 }
 
-function getImage(power, counter1, counter2, counter3, counter4, counter5, counter6, counter7, counter8, counter9) {
-    if (power >= 0 && power <= 500)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-0-500/[0-500]_DBXENFT_${counter1}.png`, value: 1 }
-    if (power > 500 && power <= 1000)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-501-1000/[500-1000]_DBXENFT_${counter2}.png`, value: 2 }
-    if (power > 1000 && power <= 2500)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-1001-2500/[1000_2500]_DBXENFT_${counter3}.png`, value: 3 }
-    if (power > 2500 && power <= 5000)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-2501-5000/[2500_5000]_DBXENFT_${counter4}.png`, value: 4 }
-    if (power > 5000 && power <= 7500)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-5001-7500/[5000-7500]_DBXENFT_${counter5}.png`, value: 5 }
-    if (power > 7500 && power <= 10000)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-7501-10000/[7501-10000]_DBXENFT_${counter6}.png`, value: 6 }
-    if (power > 10000 && power <= 12500)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-10001-12500/[10001-12500]_DBXENFT${counter7}.png`, value: 7 }
-    if (power > 12500 && power <= 15000)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-12501-15000/[12501-15000]_DBXENFT_${counter8}.png`, value: 8 }
-    if (power > 15000)
-        return { link: `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-15000/[15000]_DBXENFT_${counter9}.png`, value: 9 }
+function getImage(power, id) {
+  let imageId;
+  if (power >= 0 && power <= 500) {
+    imageId = id % 10000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-0-500/[0-500]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 500 && power <= 1000) {
+    imageId = id % 10000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-501-1000/[500-1000]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 1000 && power <= 2500) {
+    imageId = id % 10000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-1001-2500/[1000_2500]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 2500 && power <= 5000) {
+    imageId = id % 10000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-2501-5000/[2500_5000]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 5000 && power <= 7500) {
+    imageId = id % 10000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-5001-7500/[5000-7500]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 7500 && power <= 10000) {
+    imageId = id % 6000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-7501-10000/[7501-10000]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 10000 && power <= 12500) {
+    imageId = id % 6000;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-10001-12500/[10001-12500]_DBXENFT${imageId}.png`;
+  }
+  if (power > 12500 && power <= 15000) {
+    imageId = id % 3500;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-12501-15000/[12501-15000]_DBXENFT_${imageId}.png`;
+  }
+  if (power > 15000) {
+    imageId = id % 2500;
+    return `https://dbxen-be.prodigy-it-solutions.com/api/assets/deboxnft-assets-15000/[15000]_DBXENFT_${imageId}.png`;
+  }
 }
 
-cron.schedule('8 48 17 * * *', async() => {
-    await generateAfterReveal();
+cron.schedule("8 48 17 * * *", async () => {
+  await generateAfterReveal();
 });
