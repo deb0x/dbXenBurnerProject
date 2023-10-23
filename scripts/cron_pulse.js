1d6f55f66c353c83435234e4eb3a98682cfd8883
sergiu.corches <corchessergiu194@gmail.com>
2023-10-23 15:46:58 +0300

    add cron

--- scripts/cron_pulse.js
+++ scripts/cron_pulse.js
@@ -7,11 +7,12 @@ import XENFT from "./DBXENFT.js";
 import dotenv from "dotenv";
 import BigNumber from "bignumber.js";
 import fetch from "node-fetch";
-import Web3 from 'web3';
+import DBXENFTABI from "./DBXENFTABI.js";
 
 dotenv.config();
 
 const dbxenftFactoryAddress = "0xdFd373C3e3064E1D71F6E2aEDeCFE7E20B9B6044";
+const dbxenftAddress = "0x88bc12840433622b6D26D436004F553607b4C784";
 const mintInfoAddress = "0xf758E628F59C6092579DC5e30160d73B64350042";
 const xenftAddress = "0xfEa13BF27493f04DEac94f67a46441a68EfD32F8";
 
@@ -23,18 +24,19 @@ const createApiOptions = (data) => ({
   body: JSON.stringify(data),
 });
 
+const getStorageObject = (data) =>
+  fetch(
+    STORAGE_EP + "GetObjectCommand",
+    createApiOptions(data)
+  ).then((result) => result.json());
+
 const putStorageObject = (data) =>
   fetch(
     STORAGE_EP + "PutObjectCommand",
     createApiOptions(data)
   ).then((result) => result.json());
 
-function subMinutes(date, minutes) {
-  date.setMinutes(date.getMinutes() - minutes);
-  return date;
-}
-
-function mulDiv(x, y, denominator) {
+  function mulDiv(x, y, denominator) {
   const bx = new BigNumber(x);
   const by = new BigNumber(y);
   const bDenominator = new BigNumber(denominator);
@@ -44,56 +46,57 @@ function mulDiv(x, y, denominator) {
 }
 
 async function generateAfterReveal() {
-  console.log("Start cron on pulse-chain")
+  console.log("Start running on pulse");
   try {
     const provider = new JsonRpcProvider("https://rpc.pulsechain.com");
 
-    const eventSignature = '0x351a36c9c7d284a243725ea280c7ca2b2b1b02bf301dd57d03cbc43956164e78';
-    const web3 = new Web3("https://rpc.pulsechain.com");
-
-    const currentBlock = await web3.eth.getBlockNumber();
-    
-    const secondsPerBlock = 15;
-    const blocksPerHour = Math.ceil(3600 / secondsPerBlock);
-    const blocksPerDay = Math.ceil(25 * blocksPerHour);
-    const fromBlock = Math.floor(currentBlock - blocksPerDay);
-    const toBlock = 'latest';
-
-    const filter = {
-      address: dbxenftFactoryAddress,
-      topics: [eventSignature],
-    };
-
-    const mintedIds = [];
-
-    const logs = await web3.eth.getPastLogs({
-      fromBlock: fromBlock,
-      toBlock: toBlock,
-      topics: filter.topics,
-      address: filter.address,
-    });
-
-    for (const log of logs) {
-      const eventABI = [
-        { type: 'uint256', name: 'cycle', indexed: true },
-        { type: 'uint256', name: 'DBXENFTId' },
-        { type: 'uint256', name: 'XENFTID' },
-        { type: 'uint256', name: 'fee' },
-        { type: 'address', name: 'minter', indexed: true },
-      ];
-
-      const decodedData = web3.eth.abi.decodeLog(
-        eventABI,
-        log.data,
-        log.topics.slice(1)
-      );
-      mintedIds.push(Number(decodedData.DBXENFTId));
+    let fileName ="lastId.json";
+    const dbxenft = DBXENFTABI(provider, dbxenftAddress);
+    let lastMintedId = await dbxenft.totalSupply();
+    let currentId = {"lastId" : Number(lastMintedId)}
+    let myLastId;
+    let mintedIds = [];
+    const params = {
+        Bucket: METADATA_BUCKET_PULSE,
+        Key: fileName,
+    }
+    let objectData = await getStorageObject(params);
+    if(objectData.client_error != undefined ) {
+      if (objectData.client_error.Code === "NoSuchKey") {
+        const params = {
+            Bucket: METADATA_BUCKET_PULSE,
+            Key: fileName,
+            Body: JSON.stringify(currentId),
+            Tagging: 'public=yes',
+            "ContentType": "application/json",
+        };
+        putStorageObject(params)
+          .then((result) => {
+              console.log(result)
+          }).catch((error) => console.log(error));
+      } 
+    } else {
+        myLastId = Number(objectData.lastId);
+        const params = {
+          Bucket: METADATA_BUCKET_PULSE,
+          Key: fileName,
+          Body: JSON.stringify(currentId),
+          Tagging: 'public=yes',
+          "ContentType": "application/json",
+      };
+      putStorageObject(params)
+          .then((result) => {
+              console.log(result)
+          }).catch((error) => console.log(error));
+          for (let i = myLastId; i <= Number(lastMintedId); i++) {
+            mintedIds.push(i);
+          }
     }
 
     const MintInfoContract = mintInfo(provider, mintInfoAddress);
     const XENFTContract = XENFT(provider, xenftAddress);
     const factory = Factory(provider, dbxenftFactoryAddress);
-    for (let i = mintedIds.length - 1; i >= 0; i--) {
+    for (let i = 0; i < mintedIds.length; i++) {
       let XENFTID = Number(await factory.dbxenftUnderlyingXENFT(mintedIds[i]));
       let mintInforesult = await XENFTContract.mintInfo(XENFTID);
       let mintInfoData = await MintInfoContract.decodeMintInfo(mintInforesult);
@@ -154,6 +157,7 @@ async function generateAfterReveal() {
   } catch (error) {
     console.error('Error:', error);
   }
+  console.log("Finish task on pulse");
 }
 
 function getImage(power, id) {
